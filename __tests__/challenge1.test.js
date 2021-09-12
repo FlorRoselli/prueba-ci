e605acf22ad73aa4d2cbe04b0958f5573b368245
FlorRoselli <florencia.roselli@hotmail.com>
2021-09-11 21:27:07 -0300

    Agregando nueva caracteristica al proyecto

--- __tests__/challenge1.test.js
+++ __tests__/challenge1.test.js
@@ -1,5 +1,4 @@
-const { TestWatcher } = require("jest");
-const {suma} = require("../index");
+const {suma, resta} = require("../index");
 
 test("Debería regresar 8 al pasar como argumentos 6 y 2", () => {
     let a = 6;
@@ -8,4 +7,13 @@ test("Debería regresar 8 al pasar como argumentos 6 y 2", () => {
     let result = suma(a,b);
 
     expect(result).toBe(8);
+});
+
+test("Debería regresar 4 al pasar como argumentos 8 y 4", () => {
+    let a = 8;
+    let b = 4;
+
+    let result = resta(a, b);
+
+    expect(result).toBe(4);
 });
